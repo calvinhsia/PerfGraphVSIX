bdd99ec6afdce32c60e2dc802ed170a534d00647
Calvin Hsia <calvinh@microsoft.com>
2019-09-19 17:25:49 -0700

    objtrack with dispose via reflection

--- Tests/UnitTest1.cs
+++ Tests/UnitTest1.cs
@@ -5,7 +5,11 @@ using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Diagnostics;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Reflection;
 using System.Security.Cryptography;
+using System.Security.Policy;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
@@ -70,7 +74,7 @@ namespace Tests
             }
         }
 
-        class MyBigData
+        class MyBigData : IDisposable
         {
             int[] _bigArray;
             object _obj;
@@ -79,9 +83,18 @@ namespace Tests
                 _bigArray = new int[10000];
                 _obj = obj;
             }
+            bool _fDisposed { get; set; } = false;
+            public void Dispose()
+            {
+                if (!_fDisposed)
+                {
+                    _fDisposed = true;
+                }
+            }
+
             public override string ToString()
             {
-                return $"{_obj}";
+                return $"{_obj}, IsDisposed={_fDisposed}";
             }
         }
 
@@ -91,15 +104,16 @@ namespace Tests
             {
                 static int g_baseSerialNo = 0;
                 internal WeakReference<object> _wr;
-                internal string descriptor { get; private set; }
-                int _serialNo;
-                DateTime _dtCreated;
+
+                public string descriptor { get; private set; }
+                public int _serialNo;
+                public DateTime _dtCreated;
                 public ObjWeakRefData(object obj, string description)
                 {
                     _dtCreated = DateTime.Now;
                     _wr = new WeakReference<object>(obj);
-                    Interlocked.Increment(ref g_baseSerialNo);
                     _serialNo = g_baseSerialNo;
+                    Interlocked.Increment(ref g_baseSerialNo);
                     descriptor = $"{obj.GetType().Name} {description}".Trim();
                 }
                 /// <summary>
@@ -111,14 +125,43 @@ namespace Tests
                     var hasBeenClosedOrDisposed = false;
                     if (_wr.TryGetTarget(out var obj))
                     {
-                        var typeName = obj.GetType().Name;
+                        var typeName = obj.GetType().Name; //"MyBigData"
+                        bool fDidGetSpecialType = false;
                         switch (typeName)
                         {
                             case "Microsoft.VisualStudio.Text.Implementation.TextBuffer":
                                 var IsClosedProp = obj.GetType().GetProperty("IsClosed");
                                 var valIsClosedProp = IsClosedProp.GetValue(obj);
                                 hasBeenClosedOrDisposed = (bool)valIsClosedProp;
+                                fDidGetSpecialType = true;
                                 break;
+                        }
+                        if (!fDidGetSpecialType)
+                        {
+                            var mems = obj.GetType().GetMembers(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance);
+                            foreach (var mem in mems
+                                .Where(m => m.Name.IndexOf("disposed", comparisonType: StringComparison.OrdinalIgnoreCase) > 0 &&
+                                        m.MemberType.HasFlag(MemberTypes.Field) || m.MemberType.HasFlag(MemberTypes.Property)))
+                            {
+                                if (mem is PropertyInfo propinfo && propinfo.PropertyType.Name == "Boolean") // the setter has Void PropertyType
+                                {
+                                    var valIsDisposed = propinfo.GetValue(obj);
+                                    hasBeenClosedOrDisposed = (bool)valIsDisposed;
+                                    break;
+                                }
+                                else if (mem is FieldInfo fieldinfo && fieldinfo.FieldType.Name == "Boolean" && !mem.Name.Contains("BackingField"))
+                                {
+                                    var valIsDisposed = fieldinfo.GetValue(obj);
+                                    hasBeenClosedOrDisposed = (bool)valIsDisposed;
+                                    break;
+                                }
+                                else if (mem is MethodInfo methInfo && methInfo.ReturnType.Name == "Boolean")
+                                {
+                                    var valIsDisposed = methInfo.Invoke(obj, parameters: null);
+                                    hasBeenClosedOrDisposed = (bool)valIsDisposed;
+                                    break;
+                                }
+                            }
                         }
                     }
                     return hasBeenClosedOrDisposed;
@@ -138,7 +181,7 @@ namespace Tests
                 //                _hashObjs.Add(new ObjWeakRefData(obj, description));
             }
 
-            internal Dictionary<string, int> GetCounts()
+            internal (Dictionary<string, int>, List<ObjWeakRefData>) GetCounts()
             {
                 while (_queue.TryDequeue(out var obj))
                 {
@@ -154,9 +197,16 @@ namespace Tests
                 foreach (var itm in _hashObjs)
                 {
                     if (itm._wr.TryGetTarget(out var objdata))
-                    {
-                        dictLiveObjs.TryGetValue(itm.descriptor, out var cnt);
-                        dictLiveObjs[itm.descriptor] = cnt++;
+                    { // the obj is still in memory. Has it been closed or disposed?
+                        if (itm.HasBeenClosedOrDisposed())
+                        {
+                            lstLeakedObjs.Add(itm);
+                        }
+                        else
+                        {
+                            dictLiveObjs.TryGetValue(itm.descriptor, out var cnt);
+                            dictLiveObjs[itm.descriptor] = ++cnt;
+                        }
                     }
                     else
                     {
@@ -167,7 +217,7 @@ namespace Tests
                 {
                     _hashObjs.Remove(entry);
                 }
-                return dictLiveObjs;
+                return (dictLiveObjs, lstLeakedObjs);
             }
 
             public void Cleanup()
@@ -194,6 +244,74 @@ namespace Tests
             }
         }
 
+        [TestMethod]
+        public async Task TestObjTrackerWithDisposable()
+        {
+            int nThreads = 10;
+            int nIter = 1000;
+            int cnt = 0;
+            var rand = new Random(1);
+            ConcurrentBag<object> hashHardRefs = new ConcurrentBag<object>();
+            using (var objTracker = new ObjTracker())
+            {
+                try
+                {
+                    var cts = new CancellationTokenSource();
+                    var doneEvent = new ManualResetEventSlim();
+                    var tasks = new Task[nThreads];
+                    for (int iThread = 0; iThread < nThreads; iThread++)
+                    {
+                        tasks[iThread] = Task.Run(() =>
+                        {
+                            for (int i = 0; i < nIter; i++)
+                            {
+                                var obj = new MyBigData(cnt);
+                                Interlocked.Increment(ref cnt);
+                                objTracker.AddObjectToTrack(obj);
+                                if (rand.Next(100) < 50)
+                                {
+                                    obj.Dispose();
+                                }
+                                if (rand.Next(100) < 50)
+                                {
+                                    hashHardRefs.Add(obj);
+                                }
+                            }
+                        }
+                        );
+                    }
+                    await Task.Run(() => Task.WaitAll(tasks));
+                    doneEvent.Set();
+                    var res = objTracker.GetCounts();
+                    LogTestMessage($"Got results: live: {res.Item1.Count}  Leaked: {res.Item2.Count}  HardRefsCount={hashHardRefs.Count}");
+                    foreach (var live in res.Item1)
+                    {
+                        LogTestMessage($"  Live {live.Value, 3} {live.Key}");
+                    }
+                    foreach (var leak in res.Item2)
+                    {
+                        LogTestMessage($"  Leaked {leak._serialNo} {leak.descriptor}");
+                    }
+                    cts.Cancel();
+                    Assert.AreEqual(1, res.Item1.Count, "only 1 leaking type");
+                    Assert.IsTrue(res.Item1["MyBigData"] > 1000, "# live > 1000");
+                    Assert.IsTrue(res.Item2.Count > 1000, "# leaked > 1000");
+                    //                    Assert.AreEqual(nIter * nThreads, coll.Count, $" should be equal");
+
+                }
+                catch (Exception ex)
+                {
+                    LogTestMessage($"got exception {ex.ToString()}");
+                    throw;
+                }
+                //LogTestMessage($"expect {nThreads * nIter} cnt = {cnt} CollSize={coll.Count}");
+                //Assert.AreEqual(nIter * nThreads, cnt, $" items added should be equal");
+                //Assert.IsTrue(nIter * nThreads > coll.Count, $" coll count should be < because GC");
+                //Assert.AreEqual(1, coll.Count, "GC collected all but hardref");
+            }
+        }
+
+
 
         [TestMethod]
         public async Task TestObjTracker()
@@ -216,7 +334,8 @@ namespace Tests
                         {
                             for (int i = 0; i < nIter; i++)
                             {
-                                var obj = new MyBigData(Interlocked.Increment(ref cnt));
+                                var obj = new MyBigData(cnt);
+                                Interlocked.Increment(ref cnt);
                                 Interlocked.CompareExchange(ref hardref, obj, null);
                                 objTracker.AddObjectToTrack(obj);
                             }
@@ -227,8 +346,8 @@ namespace Tests
                     doneEvent.Set();
                     var res = objTracker.GetCounts();
                     cts.Cancel();
-                    Assert.AreEqual(1, res.Count, "only 1 left in hardref");
-//                    Assert.AreEqual(nIter * nThreads, coll.Count, $" should be equal");
+                    Assert.AreEqual(1, res.Item1.Count, "only 1 left in hardref");
+                    //                    Assert.AreEqual(nIter * nThreads, coll.Count, $" should be equal");
 
                 }
                 catch (Exception ex)
