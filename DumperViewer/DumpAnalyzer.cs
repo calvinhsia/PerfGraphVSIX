ba15360d02eb9cd070bbef73aee784f4631ce986
Calvin Hsia <calvinh@microsoft.com>
2019-10-22 15:55:18 -0700

    create dump

---
+++ DumperViewer/DumpAnalyzer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DumperViewer
+{
+    internal class DumpAnalyzer
+    {
+        private readonly string _dumpFileName;
+
+        public DumpAnalyzer(string dumpFileName)
+        {
+            this._dumpFileName = dumpFileName;
+        }
+
+        internal void AnalyzeDump()
+        {
+
+        }
+    }
+}
--- Tests/TestDumperViewer.cs
+++ Tests/TestDumperViewer.cs
@@ -3,6 +3,7 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,18 +14,62 @@ namespace Tests
     public class TestDumperViewer : BaseTestClass
     {
         [TestMethod]
-        public void TestDumperViewerArgs()
+        public void TestDumperInProc()
         {
             var pid = Process.GetProcessesByName("devenv")[0].Id;
             pid = Process.GetCurrentProcess().Id;
-            var args = new[] { "-p", pid.ToString() };
+            var pathDumpFile = Path.Combine(Environment.CurrentDirectory, "test dump.dmp");
+            if (File.Exists(pathDumpFile))
+            {
+                File.Delete(pathDumpFile);
+            }
+            var args = new[] { 
+                "-p", pid.ToString(),
+                "-f",  "\"" + pathDumpFile + "\""
+            };
             var odumper = new DumperViewer.DumperViewer(args)
             {
                 _logger = this
             };
             odumper.DoMain();
 
+            Assert.IsNotNull(_lstLoggedStrings.Where(s => s.Contains("in DumperViewer")).FirstOrDefault());
+
+            Assert.IsNotNull(_lstLoggedStrings.Where(s => s.Contains("Done creating dump")).FirstOrDefault());
+        }
+
+        [TestMethod]
+        public void TestDumperOutOfProc()
+        {
+            // todo
 
         }
+
+        [TestMethod]
+        public void TestDumperGetData()
+        {
+            var pid = Process.GetProcessesByName("devenv")[0].Id;
+            pid = Process.GetCurrentProcess().Id;
+            var pathDumpFile = Path.Combine(Environment.CurrentDirectory, "test dump.dmp");
+            if (File.Exists(pathDumpFile))
+            {
+                File.Delete(pathDumpFile);
+            }
+            var args = new[] {
+                "-p", pid.ToString(),
+                "-f",  "\"" + pathDumpFile + "\""
+            };
+            var odumper = new DumperViewer.DumperViewer(args)
+            {
+                _logger = this
+            };
+            odumper.DoMain();
+
+            Assert.IsNotNull(_lstLoggedStrings.Where(s => s.Contains("in DumperViewer")).FirstOrDefault());
+
+            Assert.IsNotNull(_lstLoggedStrings.Where(s => s.Contains("Done creating dump")).FirstOrDefault());
+        }
+
+
     }
 }
