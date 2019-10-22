cbea508f3c22fbf4ac3d6de46ac37c22dc4f8c9a
Calvin Hsia <calvinh@microsoft.com>
2019-10-21 18:31:42 -0700

    dumper tests

---
+++ Tests/TestDumperViewer.cs
@@ -0,0 +1,22 @@
+﻿using DumperViewer;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests
+{
+    [TestClass]
+    public class TestDumperViewer : BaseTestClass
+    {
+        [TestMethod]
+        public void TestDumperViewerArgs()
+        {
+            var args = new[] {"" };
+            var od = new DumperViewer.DumperViewer(args);
+
+        }
+    }
+}
