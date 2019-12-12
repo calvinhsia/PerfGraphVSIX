23d6b17dca8a40c74ad9bb8887971b45079a0743
Calvin Hsia <calvinh@microsoft.com>
2019-12-12 11:00:46 -0800

    add more env vars

--- Microsoft.Test.Stress/TestStressDll/TestBuildMachine.cs
+++ Microsoft.Test.Stress/TestStressDll/TestBuildMachine.cs
@@ -2,6 +2,7 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -19,9 +20,15 @@ namespace TestStressDll
         {
             logger = new Logger(new TestContextWrapper(TestContext));
             _VSHandler = new VSHandler(logger);
-            logger.LogMessage($"Username" + Environment.GetEnvironmentVariable("Username"));
-            logger.LogMessage($"UserDomain " + Environment.GetEnvironmentVariable("userdomain"));
-            logger.LogMessage($"VS Path {VSHandler.GetVSFullPath()}");
+            logger.LogMessage($"Computername=" + Environment.GetEnvironmentVariable("Computername"));
+            logger.LogMessage($"TEMP=" + Environment.GetEnvironmentVariable("TEMP"));
+            logger.LogMessage($"LOCALAPPDATA=" + Environment.GetEnvironmentVariable("LOCALAPPDATA"));
+
+            logger.LogMessage($"Username=" + Environment.GetEnvironmentVariable("Username"));
+            logger.LogMessage($"UserDomain=" + Environment.GetEnvironmentVariable("userdomain"));
+            logger.LogMessage($"ProgramFiles(x86)=" + Environment.GetEnvironmentVariable("ProgramFiles(x86)"));
+            logger.LogMessage($"Path=" + Environment.GetEnvironmentVariable("path"));
+            logger.LogMessage($"VS Path={VSHandler.GetVSFullPath()}");
 
             await _VSHandler.StartVSAsync();
             logger.LogMessage($"TestInit starting VS pid= {_VSHandler.vsProc.Id}");
@@ -30,6 +37,12 @@ namespace TestStressDll
         public async Task TestBuildMachineDTE()
         {
             await Task.Yield();
+            var vsprocs = Process.GetProcessesByName("devenv");
+            logger.LogMessage($"# of devenv = {vsprocs.Length}");
+            foreach (var devenv in vsprocs)
+            {
+                logger.LogMessage($" {devenv.MainModule.FileName}");
+            }
 
         }
         [TestCleanup]
