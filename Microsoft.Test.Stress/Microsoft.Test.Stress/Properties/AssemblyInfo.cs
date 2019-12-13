7e33790f36a309d49e24fc11c6fe5d078ac37ca5
Michael Maio <michma@microsoft.com>
2019-12-13 14:44:36 -0800

    Fix AssemblyDescription.

--- Microsoft.Test.Stress/Microsoft.Test.Stress/Properties/AssemblyInfo.cs
+++ Microsoft.Test.Stress/Microsoft.Test.Stress/Properties/AssemblyInfo.cs
@@ -7,7 +7,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Microsoft.Test.Stress")]
-[assembly: AssemblyDescription("Memory analsysis and leak detection")]
+[assembly: AssemblyDescription("Memory analysis and leak detection")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Microsoft")]
 [assembly: AssemblyProduct("Microsoft.Test.Stress")]
