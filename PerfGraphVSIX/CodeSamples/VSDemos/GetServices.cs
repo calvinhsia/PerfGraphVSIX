1a3b9c02e0b01e2887630e3778f079f7bb6cee9c
Calvin Hsia <calvinh@microsoft.com>
2020-06-11 17:45:38 -0700

    GetService Sample to Output Window

--- PerfGraphVSIX/CodeSamples/VSDemos/GetServices.cs
+++ PerfGraphVSIX/CodeSamples/VSDemos/GetServices.cs
@@ -2,25 +2,19 @@
 // This code will be compiled and run when you hit the ExecCode button. Any error msgs will be shown in the status log control.
 // This allows you to create a stress test by repeating some code, while taking measurements between each iteration.
 
-//  Macro substitution: %PerfGraphVSIX% will be changed to the fullpath to PerfGraphVSIX
-//                      %VSRoot% will be changed to the fullpath to VS: e.g. "C:\Program Files (x86)\Microsoft Visual Studio\2019\Preview"
-
-// Sample Additional references, as needed
-//Ref: %VSRoot%\Common7\IDE\PublicAssemblies\Microsoft.VisualStudio.Shell.Interop.8.0.dll
-//Ref: %VSRoot%\Common7\IDE\PublicAssemblies\Microsoft.VisualStudio.Shell.Interop.10.0.dll
-//Ref: %VSRoot%\Common7\IDE\PublicAssemblies\Microsoft.VisualStudio.Shell.Interop.11.0.dll
-//Ref: "%VSRoot%\VSSDK\VisualStudioIntegration\Common\Assemblies\v4.0\Microsoft.VisualStudio.Shell.Interop.12.1.DesignTime.dll"
-//Ref: "%VSRoot%\VSSDK\VisualStudioIntegration\Common\Assemblies\v4.0\Microsoft.VisualStudio.Shell.Interop.15.0.DesignTime.dll"
-//Ref: "%VSRoot%\VSSDK\VisualStudioIntegration\Common\Assemblies\v4.0\Microsoft.VisualStudio.Shell.Interop.15.8.DesignTime.dll"
-//Ref: %VSRoot%\Common7\IDE\PublicAssemblies\Microsoft.VisualStudio.Shell.15.0.dll
-//Ref: %VSRoot%\Common7\IDE\PublicAssemblies\Microsoft.VisualStudio.ComponentModelHost.dll
-
 //Include: ..\Util\MyCodeBaseClass.cs
 
 
 using System;
 using System.Threading;
 using System.Threading.Tasks;
+using System.Reflection;
+using System.Windows;
+using System.Xml;
+using System.Windows.Markup;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.IO;
 using Microsoft.VisualStudio;
 using Microsoft.VisualStudio.Shell;
 using Microsoft.VisualStudio.Shell.Interop;
@@ -31,12 +25,13 @@ using Task = System.Threading.Tasks.Task;
 /* This sample allows you to edit/compile/run code inside the VS process from within the same instance of VS
  * You can access VS Services, JTF, etc with the same code as you would from e.g. building a VS component
  * but the Edit/Build/Run cycle is much smaller and faster
- * rIntellisense mostly works. Debugging is via logging or output window pane.
+ * Intellisense mostly works. Debugging is mostly via logging or output window pane.
  * */
 namespace MyCodeToExecute
 {
     public class MySimpleSample : MyCodeBaseClass, IVsSelectionEvents
     {
+        public bool UseOutputPane { get; set; } = false;
 
         public IVsOutputWindowPane _OutputPane;
 
@@ -48,52 +43,87 @@ namespace MyCodeToExecute
         MySimpleSample(object[] args) : base(args) { }
         async Task DoInitializeAsync()
         {
+            CloseableTabItem tabItemTabProc = GetTabItem();
+            var strxaml =
+$@"<Grid
+xmlns=""http://schemas.microsoft.com/winfx/2006/xaml/presentation""
+xmlns:x=""http://schemas.microsoft.com/winfx/2006/xaml""
+xmlns:l=""clr-namespace:{this.GetType().Namespace};assembly={
+    System.IO.Path.GetFileNameWithoutExtension(Assembly.GetExecutingAssembly().Location)}"" 
+        >
+        <Grid.RowDefinitions>
+            <RowDefinition Height=""auto""/>
+            <RowDefinition Height=""*""/>
+        </Grid.RowDefinitions>
+        <StackPanel Grid.Row=""0"" HorizontalAlignment=""Left"" Height=""75"" VerticalAlignment=""Top"" Orientation=""Horizontal"">
+            <CheckBox Margin=""15,0,0,10"" Content=""Output To Debug OutputPane""  IsChecked=""{{Binding UseOutputPane}}""  
+                ToolTip=""Output to Debug OutputPane 'PerfGraphVSIX'""/>
+        <TextBox xml:space=""preserve"" >
+IVsMonitorSelection until this tab is closed.
+Output to Debug OutputPane or LogStatus
+</TextBox>
+        </StackPanel>
+        <Grid Name=""gridUser"" Grid.Row = ""1""></Grid>
+    </Grid>
+";
+            var strReader = new System.IO.StringReader(strxaml);
+            var xamlreader = XmlReader.Create(strReader);
+            var grid = (Grid)(XamlReader.Load(xamlreader));
+            tabItemTabProc.Content = grid;
+
+            grid.DataContext = this;
+            var gridUser = (Grid)grid.FindName("gridUser");
             _OutputPane = await GetOutputPaneAsync();
             _OutputPane.Clear();
+            uint cookieSelectionEvents = 0;
             await ThreadHelper.JoinableTaskFactory.RunAsync(async () =>
             {
+                LogMessage("Here in MySimpleSample " + DateTime.Now.ToString("MM/dd/yy hh:mm:ss"));
                 await TaskScheduler.Default; // switch to background thread
-                await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync();//outputpane must be called from main thread
-                _OutputPane.OutputString("Here in MySimpleSample " + DateTime.Now.ToString("MM/dd/yy hh:mm:ss") + "\r\n");
-                await TaskScheduler.Default; // switch to background thread
-                _OutputPane.OutputString("Logger message from MySimpleSample\r\n");
+                LogMessage("Logger message from MySimpleSample");
 
                 var SolutionService = await _asyncServiceProvider.GetServiceAsync(typeof(SVsSolution)) as IVsSolution;
-                _OutputPane.OutputString($"{nameof(SolutionService)} {SolutionService}\n");
+                LogMessage($"{nameof(SolutionService)} {SolutionService}");
 
                 var oleMenuCommandService = await _asyncServiceProvider.GetServiceAsync(typeof(IMenuCommandService)) as OleMenuCommandService;
-                _OutputPane.OutputString($"{nameof(oleMenuCommandService)} = {oleMenuCommandService}\n");
+                LogMessage($"{nameof(oleMenuCommandService)} = {oleMenuCommandService}");
 
                 var VsShellMonitorSelection = await _asyncServiceProvider.GetServiceAsync(typeof(SVsShellMonitorSelection)) as IVsMonitorSelection;
-                _OutputPane.OutputString($"{nameof(VsShellMonitorSelection)} = {VsShellMonitorSelection}\n");
-                uint cookieSelectionEvents = 0;
-                try
+                LogMessage($"{nameof(VsShellMonitorSelection)} = {VsShellMonitorSelection}");
+                VsShellMonitorSelection.AdviseSelectionEvents(this, out cookieSelectionEvents);
+                tabItemTabProc.TabItemClosed += async (o, e) =>
                 {
-                    VsShellMonitorSelection.AdviseSelectionEvents(this, out cookieSelectionEvents);
-                    while (!_CancellationTokenExecuteCode.IsCancellationRequested)
-                    {
-                        await Task.Delay(TimeSpan.FromSeconds(2), _CancellationTokenExecuteCode);
-                    }
-                }
-                catch (OperationCanceledException)
-                {
-                }
-                VsShellMonitorSelection.UnadviseSelectionEvents(cookieSelectionEvents);
+                    VsShellMonitorSelection.UnadviseSelectionEvents(cookieSelectionEvents);
+                    await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync();
+                    _perfGraphToolWindowControl.TabControl.SelectedIndex = 0;
+                };
             });
+        }
+        async Task LogMessage(string msg, params object[] args)
+        {
+            if (UseOutputPane)
+            {
+                await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync();
+                _OutputPane.OutputString(string.Format(msg, args) + Environment.NewLine);
+            }
+            else
+            {
+                _logger.LogMessage(msg, args);
+            }
         }
         public int OnCmdUIContextChanged(uint dwCmdUICookie, int fActive)
         {
-            _OutputPane.OutputString($"{nameof(OnCmdUIContextChanged)} UICtxCookie={dwCmdUICookie} fActive= {fActive}\n");
+            LogMessage($"{nameof(OnCmdUIContextChanged)} UICtxCookie={dwCmdUICookie} fActive= {fActive}");
             return VSConstants.S_OK;
         }
         public int OnElementValueChanged(uint elementid, object varValueOld, object varValueNew)
         {
-            _OutputPane.OutputString($"{nameof(OnElementValueChanged)} ElementId={elementid}  ValOld={varValueOld} ValNew={varValueNew}\n");
+            LogMessage($"{nameof(OnElementValueChanged)} ElementId={elementid}  ValOld={varValueOld} ValNew={varValueNew}");
             return VSConstants.S_OK;
         }
         public int OnSelectionChanged(IVsHierarchy pHierOld, uint itemidOld, IVsMultiItemSelect pMISOld, ISelectionContainer pSCOld, IVsHierarchy pHierNew, uint itemidNew, IVsMultiItemSelect pMISNew, ISelectionContainer pSCNew)
         {
-            _OutputPane.OutputString($"{nameof(OnSelectionChanged)}\n");
+            LogMessage($"{nameof(OnSelectionChanged)}");
             return VSConstants.S_OK;
         }
     }
