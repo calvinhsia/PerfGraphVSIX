57349c0a65e1b4f1cd3b08a951e344c33fe13dab
Calvin Hsia <calvinh@microsoft.com>
2021-02-01 13:37:09 -0800

    Update VSIX and Asm version

--- PerfGraphVSIX/Properties/AssemblyInfo.cs
+++ PerfGraphVSIX/Properties/AssemblyInfo.cs
@@ -29,6 +29,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.3")]
-[assembly: AssemblyFileVersion("1.0.0.3")]
+[assembly: AssemblyVersion("1.0.0.4")]
+[assembly: AssemblyFileVersion("1.0.0.4")]
 [assembly: InternalsVisibleTo("Tests, PublicKey=0024000004800000940000000602000000240000525341310004000001000100c93d2644dcec6c893c44629eb99ab447dc9a84a3629c72c7860a4a1d825a60ca105646c9e42326a706b0e82207c0d16994d66af42c899f978143fd8a5ff8f2bf2af88767e381ea780f95731316004b48738a17c59cda505ec2bba4cd4aa7d21eb73ad10bd4389ffc8d8d71bbf5c645afb0564e87ef592814e69296d25e8117c1")]
